12ab8b92eda67e4f2d34200afa5acf618d6d09b7
Sam Poder <39828164+sampoder@users.noreply.github.com>
2022-09-03 21:42:40 +0800

    Create easter-egg.asm

---
+++ further-reading/easter-egg.asm
@@ -0,0 +1,14 @@
+; Easter egg! To set the repository's language to Assembly, because why not?
+
+    org  0x100        ; .com files always start 256 bytes into the segment
+
+    ; int 21h is going to want...
+
+    mov  dx, msg      ; the address of or message in dx
+    mov  ah, 9        ; ah=9 - "print string" sub-function
+    int  0x21         ; call dos services
+
+    mov  ah, 0x4c     ; "terminate program" sub-function
+    int  0x21         ; call dos services
+
+    msg  db 'Hello, World!', 0x0d, 0x0a, '$'   ; $-terminated message
